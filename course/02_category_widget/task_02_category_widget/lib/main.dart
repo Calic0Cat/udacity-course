3d70f83ac382a57f002ed5a26f06c3cd6d4b49d5
calic0cat <suzuki11235813@gmail.com>
2021-06-16 01:06:43 +0900

    add:task2

--- course/02_category_widget/task_02_category_widget/lib/main.dart
+++ course/02_category_widget/task_02_category_widget/lib/main.dart
@@ -4,7 +4,6 @@
 
 // You can read about packages here: https://flutter.io/using-packages/
 import 'package:flutter/material.dart';
-
 // You can use a relative import, i.e. `import 'category.dart';` or
 // a package import, as shown below.
 // More details at http://dart-lang.github.io/linter/lints/avoid_relative_lib_imports.html
@@ -32,7 +31,12 @@ class UnitConverterApp extends StatelessWidget {
         backgroundColor: Colors.green[100],
         body: Center(
           // TODO: Determine what properties you'll need to pass into the widget
-          child: Category(),
+          child: Category(
+            name:_categoryName,
+            color: _categoryColor,
+            iconLocation: _categoryIcon,
+
+          ),
         ),
       ),
     );
