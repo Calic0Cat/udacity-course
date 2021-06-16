ebc55cc855630f3b0717ca9d541e6562c358778f
calic0cat <suzuki11235813@gmail.com>
2021-06-16 16:24:02 +0900

    add:task03

--- course/03_category_route/task_03_category_route/lib/category_route.dart
+++ course/03_category_route/task_03_category_route/lib/category_route.dart
@@ -5,9 +5,10 @@
 import 'package:flutter/material.dart';
 
 // TODO: Check if we need to import anything
+import 'package:task_03_category_route/category.dart';
 
 // TODO: Define any constants
-
+final _backgroundColor = Colors.green[100];
 /// Category Route (screen).
 ///
 /// This is the 'home' screen of the Unit Converter. It shows a header and
@@ -39,18 +40,42 @@ class CategoryRoute extends StatelessWidget {
     Colors.purpleAccent,
     Colors.red,
   ];
+  Widget _buildCategoryWidgets(List<Widget> categories){
+    return ListView.builder(itemBuilder: (BuildContext context, int index) => categories[index],
+    itemCount: categories.length,);
+  }
 
   @override
   Widget build(BuildContext context) {
+    final categories = <Category>[];
     // TODO: Create a list of the eight Categories, using the names and colors
     // from above. Use a placeholder icon, such as `Icons.cake` for each
     // Category. We'll add custom icons later.
+    for(var i = 0; i < _categoryNames.length; i++){
+      categories.add(Category(name: _categoryNames[i], color:_baseColors[i], iconLocation: Icons.cake));
+    }
 
     // TODO: Create a list view of the Categories
-    final listView = Container();
+    final listView = Container(
+      color: _backgroundColor,
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      child: _buildCategoryWidgets(categories),
+    );
+
 
     // TODO: Create an App Bar
-    final appBar = AppBar();
+    final appBar = AppBar(
+      elevation: 0.0,
+      title: Text(
+        "Unit Converter",
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 30.0
+        ),
+      ),
+      centerTitle: true,
+      backgroundColor: _backgroundColor,
+    );
 
     return Scaffold(
       appBar: appBar,
